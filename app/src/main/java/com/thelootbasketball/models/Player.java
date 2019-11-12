e9b2ec7464dd7a74f2a5c237dde952d3e7979f07
Bryan Wong <BWong@allieddigital.net>
2019-11-11 19:31:17 -0800

    minor change

--- app/src/main/java/com/thelootbasketball/models/Player.java
+++ app/src/main/java/com/thelootbasketball/models/Player.java
@@ -2,10 +2,6 @@ package com.thelootbasketball.models;
 
 import com.google.gson.annotations.SerializedName;
 
-import java.util.ArrayList;
-import java.util.List;
-
-
 public class Player {
 
     @SerializedName("name")
