974c82ac5212c26c880baaee7563a967514765dd
Bryan Wong <BWong@allieddigital.net>
2019-12-02 19:06:11 -0800

    Display current Week of in schedule

--- app/src/main/java/com/thelootbasketball/ui/schedule/ScheduleFragment.java
+++ app/src/main/java/com/thelootbasketball/ui/schedule/ScheduleFragment.java
@@ -4,25 +4,39 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.thelootbasketball.R;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
+
 
 public class ScheduleFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState){
         final View root = inflater.inflate(R.layout.fragment_schedule, container, false);
-         final TextView textView = root.findViewById(R.id.week_of);
+        final TextView textView = root.findViewById(R.id.week_of);
+        final Button previousButton = root.findViewById(R.id.button);
 
-        Calendar cal = Calendar.getInstance();
-        int WeekOfMonth = cal.get(Calendar.WEEK_OF_MONTH);
+        Calendar cal = Calendar.getInstance(Locale.getDefault());
+        SimpleDateFormat df = new SimpleDateFormat("MM/dd/yy", Locale.getDefault());
+        //get first day of the week
+        cal.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
+        final String FirstDayOfWeek = df.format(cal.getTime());
+        //get the last day of the week
+        cal.add(Calendar.DATE, 6);
+        final String LastDayOfWeek = df.format(cal.getTime());
 
-        textView.setText(Integer.toString(WeekOfMonth));
+
+        textView.setText("Week of: " + FirstDayOfWeek + " to "+ LastDayOfWeek);
+
 
         return root;
     }
