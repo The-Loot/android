d4b7cc8e7e89a1cb2fad5985e052d48947f888e4
Bryan <bryanw05@hotmail.com>
2020-01-13 22:08:48 -0800

    added admin and setting activities

---
+++ app/src/main/java/com/thelootbasketball/ui/admin/AdminLogin.java
@@ -0,0 +1,14 @@
+package com.thelootbasketball.ui.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.thelootbasketball.R;
+import android.os.Bundle;
+
+public class AdminLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_login);
+    }
+}
---
+++ app/src/main/java/com/thelootbasketball/ui/appsettings/AppSettings.java
@@ -0,0 +1,14 @@
+package com.thelootbasketball.ui.appsettings;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.thelootbasketball.R;
+import android.os.Bundle;
+
+public class AppSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_settings);
+    }
+}
